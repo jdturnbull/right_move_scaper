5f31ab79c9d60553fe836ff6a70552420feb8f27
James Turnbull <jdturnbull98@gmail.com>
2021-07-08 13:15:47 +0100

    initial commit

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const fs = require("fs");
 
 // const purchaseUrl = `https://www.rightmove.co.uk/property-for-sale/find.html?minBedrooms=3&maxBedrooms=3&keywords=&sortType=2&viewType=LIST&channel=BUY&index=${index}&radius=0.0&locationIdentifier=USERDEFINEDAREA%5E%7B%22id%22%3A%226850088%22%7D`;
 
+// warrick url rent = https://www.rightmove.co.uk/property-to-rent/find.html?keywords=&sortType=2&viewType=LIST&channel=RENT&index=${index}&radius=0.0&locationIdentifier=USERDEFINEDAREA%5E%7B%22polylines%22%3A%22ofs%7EHdxuHivAk%7C%40%7Dk%40_Soa%40yjAs%5BqyB%60G_qBrc%40_rCfmAqgBvyAuEhiAjoBvt%40zmGkqCvkG%22%7D
+// birmingham url purchase = https://www.rightmove.co.uk/property-for-sale/find.html?minBedrooms=3&maxBedrooms=3&keywords=&sortType=2&viewType=LIST&channel=BUY&index=0&radius=0.0&locationIdentifier=USERDEFINEDAREA%5E%7B%22id%22%3A%226852065%22%2C+%22polylines%22%3A%22oed_IltrJhsAsNjd%40doEgq%40jkBkz%40feAmiAk_Aa%5Eo%7BB%7Ca%40yoCf%5Ccf%40f%5Cr%40%22%7D
+// warrick url purchase = https://www.rightmove.co.uk/property-for-sale/find.html?minBedrooms=3&maxBedrooms=3&keywords=&sortType=2&viewType=LIST&channel=BUY&index=${index}&radius=0.0&locationIdentifier=USERDEFINEDAREA%5E%7B%22id%22%3A%226850088%22%7D
+
 const getRentPricesByPage = async (page) => {
   const index = page * 24;
 
@@ -39,7 +43,7 @@ const getPurchasePricesByPage = async (page) => {
 
   const {data} = await axios({
     method: 'GET',
-    url: `https://www.rightmove.co.uk/property-for-sale/find.html?minBedrooms=3&maxBedrooms=3&keywords=&sortType=2&viewType=LIST&channel=BUY&index=${index}&radius=0.0&locationIdentifier=USERDEFINEDAREA%5E%7B%22id%22%3A%226850088%22%7D`
+    url: `https://www.rightmove.co.uk/property-for-sale/find.html?minBedrooms=3&maxBedrooms=3&keywords=&sortType=2&viewType=LIST&channel=BUY&index=${index}&radius=0.0&locationIdentifier=USERDEFINEDAREA%5E%7B%22id%22%3A%226852065%22%2C+%22polylines%22%3A%22oed_IltrJhsAsNjd%40doEgq%40jkBkz%40feAmiAk_Aa%5Eo%7BB%7Ca%40yoCf%5Ccf%40f%5Cr%40%22%7D`
   })
 
   const $ = cheerio.load(data);
@@ -105,7 +109,7 @@ const getAveragePrice = (ar) => {
 }
 
 const runApp = async () => {
-  let numberOfPages = 10;
+  let numberOfPages = 30;
 
   const rentalPrices = await getRentalPrices(numberOfPages);
   const purchasePrices = await getPurchasePrices(numberOfPages);
